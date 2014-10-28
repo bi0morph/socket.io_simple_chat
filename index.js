4acd0d17b94e1d6c99ad13c22bd0ec9e44d897ef
Ruslan Ustyuhov <rusustyugov@gmail.com>
2014-10-28 19:59:24 +0200

    Add broadcasting. Complite tutorial by socket.io simple chat

--- index.js
+++ index.js
@@ -7,14 +7,31 @@ app.get('/', function(req, res){
 });
 io.on('connection', function(socket){
 	console.log('a user connected');
+	socket.broadcast.emit('hi');
 	socket.on('disconect', function(){
 		console.log('user disconected');
 	});
 	socket.on('chat messages', function(msg){
 		console.log('message: ' + msg);
+		io.emit('chat message', msg);
 	});
 });
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+
+/*
+TODO: Homework
+
+
+Here are some ideas to improve the application:
+
+Broadcast a message to connected users when someone connects or disconnects
+Add support for nicknames
+Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+Add “{user} is typing” functionality
+Show who’s online
+Add private messaging
+Share your improvements!
+*/
